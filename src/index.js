482318dff152555c47edf39919c0e5a0ac859d54
oana-85 <78343561+oana-85@users.noreply.github.com>
2021-03-29 20:09:46 +0100

    adjust wind and humidity

--- src/index.js
+++ src/index.js
@@ -52,6 +52,10 @@ function displayWeather(response) {
   document.querySelector("h2").innerHTML = response.data.name;
   celsiusTemp = response.data.main.temp;
   document.querySelector("#temperature").innerHTML = Math.round(celsiusTemp);
+  
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
   document
