53c840411348425b389ffb9884a0ea1e54cffd0f
oana-85 <78343561+oana-85@users.noreply.github.com>
2021-04-07 07:22:00 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,65 +1,74 @@
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+  let date = now.getDate();
+  let year = now.getFullYear();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[now.getMonth()];
+  return `${day}, <br /> ${date} ${month} ${year}`;
+}
+function formatHours(timestamp) {
+  let time = new Date(timestamp);
+  let hours = time.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
 let now = new Date();
 let weekday = document.querySelector("#date-time.col-6.h1");
-let date = now.getDate();
-let year = now.getFullYear();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-let month = months[now.getMonth()];
-weekday.innerHTML = `${day}, </br> ${date} ${month} ${year}`;
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
+weekday.innerHTML = formatDate(now);
+//time//
+let time = document.querySelector("#time");
+time.innerHTML = formatHours(now);
 function formatDay(timestamp) {
-let date = new Date(timestamp * 1000);
-let day = date.getDay();
-let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
- return days[day];
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
 }
-
 function displayForecast(response) {
-let forecast =  response.data.daily;
-let forecastElement = document.querySelector("#forecast");
-let forecastHTML = `<div class = "row">`;
-
-forecast.forEach(function (forecastDay, index) {
-if(index< 6){
-forecastHTML =
-forecastHTML+ 
-`<div class="col-sm-2">
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class = "row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-sm-2">
         <div class="card">
             <div class="card-body">
                 <div class="weather-forecast-date">
                 ${formatDay(forecastDay.dt)}</div>
               <img class= "dailyForecastIcon" src="https://openweathermap.org/img/wn/${
-    forecastDay.weather[0].icon}@4x.png" alt="" width="42"/>
+                forecastDay.weather[0].icon
+              }@4x.png" alt="" width="42"/>
               <div class = "weather-forecast-temperatures">
                 <span class="weather-forecast-temperature-max">
                 ${Math.round(forecastDay.temp.max)}</span> /
@@ -70,16 +79,11 @@ forecastHTML+
             </div>
         </div>
     </div>`;
-}
+    }
   });
-   
-forecastHTML = forecastHTML +`</div>`;
-forecastElement.innerHTML = forecastHTML;
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
-
-//time//
-let time = document.querySelector("#time");
-time.innerHTML = `${hours}:${minutes}`;
 function searchInput(event) {
   event.preventDefault();
   let currentPlace = document.querySelector("#place");
@@ -88,30 +92,28 @@ function searchInput(event) {
 }
 let enteredPlace = document.querySelector("#search");
 enteredPlace.addEventListener("submit", searchInput);
-
-function getForecast(coordinates){
+function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "9d3ea23f6bf145bbb0d156ccb1b96e37"
+  let apiKey = "9d3ea23f6bf145bbb0d156ccb1b96e37";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
 function displayWeather(response) {
   document.querySelector("h2").innerHTML = response.data.name;
   celsiusTemp = response.data.main.temp;
   document.querySelector("#temperature").innerHTML = Math.round(celsiusTemp);
-  
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
   document
     .querySelector("#icon")
     .setAttribute("src", getIcon(response.data.weather[0].icon));
-
-    getForecast(response.data.coord);
-  }
+  time.innerHTML = formatHours(response.data.dt * 1000);
+  getForecast(response.data.coord);
+}
 function getIcon(icon) {
   let iconElement = "";
   if (icon === "01d" || icon === "01n") {
@@ -170,30 +172,24 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(retrievePosition);
 }
-
-function showFahrenheit(event){
+function showFahrenheit(event) {
   event.preventDefault();
-  let fahrenheitTemp = (celsiusTemp*9)/5+32;
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(fahrenheitTemp);
 }
-
-function showCelsius(event){
+function showCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemp);
 }
-
 let celsiusTemp = null;
-
 let searchForm = document.querySelector("#search");
 searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location");
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click",showFahrenheit);
-
-let celsiusLink=document.querySelector("#celsius-link");
+fahrenheitLink.addEventListener("click", showFahrenheit);
+let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsius);
 searchPlace("Arbroath");
